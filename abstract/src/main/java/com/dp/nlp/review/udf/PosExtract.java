c1d5abf39e77bd9a443ad2bcfeecba0cfcf35bfb
王强 <qiang.wang@dianping.com>
2015-03-02 13:58:41 +0800

    bug fixed.

--- abstract/src/main/java/com/dp/nlp/review/udf/PosExtract.java
+++ abstract/src/main/java/com/dp/nlp/review/udf/PosExtract.java
@@ -7,16 +7,15 @@ import org.apache.hadoop.hive.ql.exec.UDF;
  */
 public class PosExtract extends UDF {
     public static void main(String[] args) {
-        String str1 = "[\"沙拉不错:33_45:1:1\",\"性价比较高:0_11:1:1\",\"口味一般:21_27:-1:1\"]";
+        String[] arr = {"沙拉不错:33_45:1:1", "性价比较高:0_11:1:1", "口味一般:21_27:-1:1"};
         PosExtract obj = new PosExtract();
-        System.out.println(obj.evaluate(str1));
+        System.out.println(obj.evaluate(arr));
     }
 
-    public String evaluate(String str) {
-        String[] arr1 = str.split(",");
+    public String evaluate(String[] arr) {
         StringBuilder sb = new StringBuilder();
 
-        for(String x : arr1) {
+        for(String x : arr) {
             String[] arr2 = x.split(":");
             if(arr2.length == 4) {
                 sb.append("\"").append(arr2[1]).append("\"").append(" ");
--- abstract/src/main/java/com/dp/nlp/review/udf/TagExtract.java
+++ abstract/src/main/java/com/dp/nlp/review/udf/TagExtract.java
@@ -8,15 +8,13 @@ import org.apache.hadoop.hive.ql.exec.UDF;
 public class TagExtract extends UDF {
 
     public static void main(String[] args) {
-        String str1 = "[\"沙拉不错:33_45:1:1\",\"性价比较高:0_11:1:1\",\"口味一般:21_27:-1:1\"]";
+        String[] arr = {"沙拉不错:33_45:1:1", "性价比较高:0_11:1:1", "口味一般:21_27:-1:1"};
         TagExtract obj = new TagExtract();
-        System.out.println(obj.evaluate(str1));
+        System.out.println(obj.evaluate(arr));
     }
 
-    public String evaluate(String str) {
-        String[] arr = str.split(",");
+    public String evaluate(String[] arr) {
         StringBuilder sb = new StringBuilder();
-
         for(String x : arr) {
             int i =  x.indexOf(":");
             if(i >= 0) {
