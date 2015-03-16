48f036bedb5e551163bfc1c84c7ee94e2c4f0d36
王强 <qiang.wang@dianping.com>
2015-03-16 18:16:51 +0800

    hair dictionary is added.

---
+++ abstract/src/main/java/com/dp/nlp/review/udf/EntityParser.java
@@ -0,0 +1,15 @@
+package com.dp.nlp.review.udf;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+/**
+ * Created by qiangwang on 15/3/13.
+ */
+public class EntityParser extends UDF {
+
+    public String evaluate(String review, int hasPOS) {
+        return "";
+    }
+
+}
+
---
+++ abstract/src/main/java/com/dp/nlp/review/udf/ShopCategory.java
@@ -0,0 +1,17 @@
+package com.dp.nlp.review.udf;
+
+/**
+ * Created by qiangwang on 15/3/13.
+ */
+public class ShopCategory {
+    private static final int BEAUTY = 50;
+    private static final int HAIR = 157;
+    private static final int DANCE = 149;
+    private static final int MANICURE = 160;
+    private static final int YOGA = 148;
+    private static final int COSMETIC = 123;
+    private static final int FACE = 158;
+    private static final int RESHAPE = 183;
+    private static final int TEETH = 182;
+    private static final int SLIM = 159;
+}
