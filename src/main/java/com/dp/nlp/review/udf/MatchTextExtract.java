68c250ab05db582587cbeb7a763f10e8f96da488
王强 <qiang.wang@dianping.com>
2015-03-25 17:41:50 +0800

    bug fixed.

--- src/main/java/com/dp/nlp/review/udf/MatchTextExtract.java
+++ src/main/java/com/dp/nlp/review/udf/MatchTextExtract.java
@@ -11,7 +11,7 @@ public class MatchTextExtract extends UDF {
 
     public static void main(String[] args) {
         ArrayList<String> arr = new ArrayList<String>();
-        arr.add("沙拉不错:33_45:1:1:后来又\n开\"始说\"你们\t态度\n好点");
+        arr.add("沙拉不错:33_45:1:1:后 来 \\ 又 \n开\"始说\"你们\t态度\n好点");
         arr.add("性价比较高:0_11:1:1:鸡肉也很鲜美");
         arr.add("口味一般:21_27:-1:1:鸡肉也很鲜美");
         MatchTextExtract obj = new MatchTextExtract();
@@ -26,7 +26,7 @@ public class MatchTextExtract extends UDF {
             if(arr1.length != 5) {
                 continue;
             }
-            String m = arr1[4].replaceAll("\\r|\\t|\\n| |\"", "");
+            String m = arr1[4].replaceAll("\\r|\\t|\\n| |\"|\\\\", "");
             sb.append(m).append(" ");
         }
         sb.insert(sb.length() - 1, '"');
